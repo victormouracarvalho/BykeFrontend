2ee894d3fa1b3749bbb8eb391e2c56a57523b57f
arno <arno@dupere.me>
2022-11-30 17:51:43 +0100

    added vuex store

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
 import './assets/main.css'
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+createApp(App)
+    .use(router)
+    .use(store)
+    .mount('#app')
