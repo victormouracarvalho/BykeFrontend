5736272b48a14075c1f56c2feb18d544ef838a98
arnopensource <34061508+arnopensource@users.noreply.github.com>
2023-01-13 19:51:33 +0100

    bootstrap components, nice landing page, deleted useless files

--- src/App.vue
+++ src/App.vue
@@ -1,98 +1,31 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
 </script>
 
 <script lang="ts">
   import { mapGetters } from "vuex";
+  import bootstrapIcons from 'bootstrap-icons/bootstrap-icons.svg?raw'
+  import BootstrapFooter from "@/components/BootstrapFooter.vue";
 
   export default {
     name: "App",
     computed: {
       ...mapGetters(['isAuthenticated'])
+    },
+    data() {
+      return {
+        bootstrapIcons
+      }
+    },
+    components: {
+      BootstrapFooter,
     }
   }
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/login" v-if="isAuthenticated === false">Connexion</RouterLink>
-        <RouterLink to="/profile" v-if="isAuthenticated">Profil</RouterLink>
-        <RouterLink to="/sorties" v-if="isAuthenticated">Sorties</RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
+  <BootstrapFooter name="Projet Byke" year="2023"></BootstrapFooter>
+  <!-- inject bootstrap icons svg file -->
+  <div v-html="bootstrapIcons" style="display: none;"></div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,18 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+<script lang="ts">
+import BootstrapHero from "@/components/BootstrapHero.vue";
+import BootstrapFeatures from "@/components/BootstrapFeatures.vue";
+export default {
+  name: "HomeView",
+  components: {
+    BootstrapFeatures,
+    BootstrapHero,
+  },
+}
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <BootstrapHero img="/assets/geojango-maps-Z8UgB80_46w-unsplash.jpg"></BootstrapHero>
+    <BootstrapFeatures></BootstrapFeatures>
   </main>
 </template>
