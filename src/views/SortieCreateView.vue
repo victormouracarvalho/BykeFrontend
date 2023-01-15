6b5da168fbca8254cb85704b7944da6dd9c75649
arnopensource <34061508+arnopensource@users.noreply.github.com>
2023-01-15 17:26:03 +0100

    sortie create fix

--- src/views/SortieCreateView.vue
+++ src/views/SortieCreateView.vue
@@ -2,45 +2,30 @@
   <!--  Title for the page with table-->
   <div>
     <div class="container">
-      <h1>nouveau Sortie</h1>
+      <h1>Nouvelle sortie</h1>
       <div class="input-group mb-3">
         <div class="input-group-prepend">
           <span class="input-group-text" id="inputGroup-sizing-default">Arrival</span>
         </div>
-        <input type="text" id="arrival" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" >
+        <input type="text" id="arrival" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" v-model="excursion.arrival" >
       </div>
 
       <div class="input-group mb-3">
         <div class="input-group-prepend">
           <span class="input-group-text" id="inputGroup-sizing-default">BykeId</span>
         </div>
-        <input type="text" id="bikeId" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+        <input type="text" id="bikeId" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" v-model="excursion.bykeId">
       </div>
 
       <div class="input-group mb-3">
         <div class="input-group-prepend">
           <span class="input-group-text" id="inputGroup-sizing-default">Departure</span>
         </div>
-        <input type="text" id="departure" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" >
+        <input type="text" id="departure" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" v-model="excursion.departure">
       </div>
 
-<!--      <div class="input-group mb-3">-->
-<!--        <div class="input-group-prepend">-->
-<!--          <span class="input-group-text" id="inputGroup-sizing-default">id</span>-->
-<!--        </div>-->
-<!--        <input type="text" id="id" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" >-->
-<!--      </div>-->
-
-<!--      <div class="input-group mb-3">-->
-<!--        <div class="input-group-prepend">-->
-<!--          <span class="input-group-text" id="inputGroup-sizing-default">Start</span>-->
-<!--        </div>-->
-<!--        <input type="text" id="start" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" >-->
-<!--      </div>-->
-
       <div >
-        <button type="button" class="btn btn-primary mr-2" @click="Create()">Add</button>
-        <!--      <button type="button" class="btn btn-danger mr-2" @click="Delete(excursion.id)">Delete</button>-->
+        <button type="button" class="btn btn-primary mr-2" @click="create">Add</button>
       </div>
 
     </div>
@@ -50,34 +35,30 @@
 
 
 <script>
-// import {v4 as uuidv4} from 'uuid';
 import ApiService from "../common/api.service";
 export default {
   async created() {
     if (this.$store.getters.isAuthenticated === false) {
       this.$router.push({name: "login"})
-      return
     }
-
   },
   data() {
     return {
       sorties: null,
+      excursion: {
+        "id": 0,
+        "bykeId": 0,
+        "departure": "",
+        "arrival": "",
+        "start": "",
+      }
     };
   },
   methods:{
-    Create(){
-      const excursion = {
-        "arrival": document.getElementById("arrival").value,
-        "bikeId": + document.getElementById("bikeId").value,
-        "departure": document.getElementById("departure").value,
-        "id": 1, // changer après dans le backend
-        "start": document.getElementById("start").value,
-      }
-      ApiService.excursions.create(excursion)
-      // window.location.href = "/";
+    async create(){
+      await ApiService.excursions.create(this.excursion)
+      this.$router.push({name: "sortie-list"})
     }
   },
-
 };
 </script>
--- src/views/SortieListView.vue
+++ src/views/SortieListView.vue
@@ -53,7 +53,7 @@ export default {
   },
   data() {
     return {
-      sorties: null,
+      excursions: [],
     };
   },
   methods:{
